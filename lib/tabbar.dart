c7c51658b19be4c35290402033e1b7f837e9cff9
jingjun <254032134@qq.com>
2020-01-07 17:42:17 +0800

    feat: 创建控制器

---
+++ lib/tabbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class Tabbar extends StatefulWidget {
+  @override
+  tabbarState createState() {
+    return new tabbarState();
+  }
+}
+
+class tabbarState extends State<Tabbar> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+  }
+}
