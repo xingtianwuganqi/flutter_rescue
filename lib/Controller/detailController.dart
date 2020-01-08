020cb1a280b4bbd385d578f0cc3c7871964c2d74
jingjun <254032134@qq.com>
2020-01-08 17:30:00 +0800

    feat: 列表组头部

--- lib/Controller/detailController.dart
+++ lib/Controller/detailController.dart
@@ -50,7 +50,7 @@ class detailStates extends State<detailController> {
       ),
       child: Container(
           height: 5.0,
-          child: blueBox
+          child: RowBox
       ),
     );
   }
@@ -70,23 +70,84 @@ class detailStates extends State<detailController> {
       itemCount: 100,
       itemExtent: 50.0, //强制高度为50.0
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(title: Text("$index"));
+//        return ListTile(title: Text("$index"));
+        print(index);
+        return index % 10 == 0 ? Container(
+          color: Colors.grey.withOpacity(0.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Text("Header",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text("Feb 26th, 2019",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.black45,
+                  )),
+            ],
+          ),
+          padding: EdgeInsets.all(10.0),
+        )
+            : ListTile(title: Text("$index"));
       }
   );
 
-  Widget timeBox = Column(
+  Widget RowBox = Row(
     crossAxisAlignment: CrossAxisAlignment.center,
     children: <Widget>[
-      Padding(padding: EdgeInsets.only(left: 20,top: 10,right: 20,bottom: 10),
-        child: Text("2020年",textAlign: TextAlign.left,style: TextStyle(color: Colors.white,fontSize: 12),),
+      Expanded(
+        flex: 1,
+        child: blueBox,
       ),
-      
-
+      Expanded(
+        flex: 1,
+        child: monthBox("收入","0.0"),
+      ),
+      Expanded(
+        flex: 1,
+        child: monthBox("支出", "3801.0"),
+      )
     ],
   );
 
+  static Widget monthBox(title,time) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.blue),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(left: 10,top: 10,right: 10,bottom: 5),
+            child: Text(title,textAlign: TextAlign.left,style: TextStyle(color: Colors.white,fontSize: 12),),
+          ),
+          Padding(padding: EdgeInsets.only(left: 10,top: 5,right: 10,bottom: 5,),
+            child: Text(time,
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.white,fontSize: 14),),
+          )
+        ],
+      ),
+    );
+  }
 
-  Widget blueBox=DecoratedBox(
+
+  static Widget blueBox=DecoratedBox(
     decoration: BoxDecoration(color: Colors.blue),
+    child: new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(padding: EdgeInsets.only(left: 20,top:10,right: 10,bottom: 5),
+          child: Text("2020年",textAlign: TextAlign.left,style: TextStyle(color: Colors.white,fontSize: 12),),
+        ),
+        Padding(padding: EdgeInsets.only(left: 20,top: 5,right: 10,bottom: 5,),
+            child: Text("01月",
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.white,fontSize: 14),)
+        )
+      ],
+    )
   );
 }
--- lib/tabbar.dart
+++ lib/tabbar.dart
@@ -38,22 +38,52 @@ class tabbarState extends State<Tabbar> {
         index: _selectedIndex,
         children: pages,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home),title: Text("明细")),
-          BottomNavigationBarItem(icon: Icon(Icons.insert_chart),title: Text("图表")),
-          BottomNavigationBarItem(icon: Icon(Icons.library_add),title: Text("添加")),
-          BottomNavigationBarItem(icon: Icon(Icons.mode_comment),title: Text("社区")),
-          BottomNavigationBarItem(icon: Icon(Icons.my_location),title: Text("我的")),
-
-        ],
-        currentIndex: _selectedIndex,
-        fixedColor: Colors.blue,
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.grey,
-        elevation: 2.0,
-        onTap: _onItemTapped,
+      bottomNavigationBar:
+      BottomAppBar(
+        color: Colors.white,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.home),onPressed: (){
+              _onItemTapped(0);
+            }),
+            IconButton(icon: Icon(Icons.insert_chart),onPressed: () {
+              _onItemTapped(1);
+            },),
+            SizedBox(),
+            IconButton(icon: Icon(Icons.mode_comment),onPressed: (){
+              _onItemTapped(3);
+            },),
+            IconButton(icon: Icon(Icons.my_location),onPressed: () {
+              _onItemTapped(4);
+            },),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        ),
       ),
+      floatingActionButton: FloatingActionButton( //悬浮按钮
+          child: Icon(Icons.add),
+          onPressed: () {
+            _onItemTapped(2);
+          },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//      BottomNavigationBar(
+//        items: <BottomNavigationBarItem>[
+//          BottomNavigationBarItem(icon: Icon(Icons.home),title: Text("明细")),
+//          BottomNavigationBarItem(icon: Icon(Icons.insert_chart),title: Text("图表")),
+//          BottomNavigationBarItem(icon: Icon(Icons.library_add),title: Text("添加")),
+//          BottomNavigationBarItem(icon: Icon(Icons.mode_comment),title: Text("社区")),
+//          BottomNavigationBarItem(icon: Icon(Icons.my_location),title: Text("我的")),
+//
+//        ],
+//        currentIndex: _selectedIndex,
+//        fixedColor: Colors.blue,
+//        type: BottomNavigationBarType.fixed,
+//        unselectedItemColor: Colors.grey,
+//        elevation: 2.0,
+//        onTap: _onItemTapped,
+//      ),
     );
   }
 
